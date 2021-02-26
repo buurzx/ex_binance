1288917b46d2ad0296d8f9289abafa98c0520a6b
Pavel Ro <buurzx@gmail.com>
2021-02-26 12:19:27 +0300

    remove unused creds

--- lib/ex_binance/private.ex
+++ lib/ex_binance/private.ex
@@ -1,12 +1,12 @@
 defmodule ExBinance.Private do
-  import ExBinance.Rest.HTTPClient, only: [get_auth: 3]
+  import ExBinance.Rest.HTTPClient, only: [get_auth: 2]
 
   @type credentials :: ExBinance.Credentials.t()
   @type shared_errors :: ExBinance.Rest.HTTPClient.shared_errors()
 
   @spec account(credentials) :: {:ok, term} | {:error, shared_errors}
-  def account(credentials, futures \\ true) do
-    with {:ok, data} <- get_auth(path(futures), %{}, credentials) do
+  def account(futures \\ true) do
+    with {:ok, data} <- get_auth(path(futures), %{}) do
       {:ok, ExBinance.Account.new(data)}
     end
   end
@@ -18,7 +18,6 @@ defmodule ExBinance.Private do
                 quantity,
                 price,
                 time_in_force,
-                credentials,
                 futures \\ true
               ),
               to: ExBinance.Rest.CreateOrder
@@ -26,12 +25,11 @@ defmodule ExBinance.Private do
   defdelegate cancel_order_by_order_id(
                 symbol,
                 order_id,
-                credentials,
                 futures \\ true
               ),
               to: ExBinance.Rest.CancelOrder
 
-  defdelegate cancel_all_orders(symbol, credentials, futures), to: ExBinance.Rest.CancelAllOrders
+  defdelegate cancel_all_orders(symbol, futures), to: ExBinance.Rest.CancelAllOrders
 
   def path(futures) do
     if futures, do: "/fapi/v2/account", else: "/api/v3/account"
